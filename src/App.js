e6c3286f4ce10895719e8cf3e7859bf2503c2a00
RadimYassin <radimyassin20@gamil.com>
2023-01-18 00:03:53 +0100

    create style.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Form from './component/Form';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Form/>
     </div>
   );
