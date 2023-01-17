9b016d472fbd4f9966fc67ed8d6b748cb884c04e
RadimYassin <radimyassin20@gamil.com>
2023-01-17 23:58:26 +0100

    create fichier Form in component

---
+++ src/component/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+           <form>
+            <input type={'text'} />
+            <button>submit</button>
+           </form>
+    </div>
+  )
+}
+
+export default Form
